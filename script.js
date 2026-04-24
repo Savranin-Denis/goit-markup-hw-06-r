fa9665ebbdba4894622e860eba3c9928eab3fead
Savranin-Denis <savranindenisv@gmail.com>
2026-04-24 19:22:25 +0300

    add mobile modale window

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const heroBtn = document.querySelector('.hero-btn');
 const modalOverlay = document.querySelector('.modal-overlay');
 const modalCloseBtn = document.querySelector('.modal-close-btn');
 
-heroBtn.addEventListener('click', () => {
-  modalOverlay.classList.add('is-open');
-});
+const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+const mobileOverlay = document.querySelector('.mobile-overlay');
+const mobileCloseBtn = document.querySelector('.mobile-close-btn');
+
+if (heroBtn) {
+  heroBtn.addEventListener('click', () => {
+    modalOverlay.classList.add('is-open');
+  });
+}
 
 modalCloseBtn.addEventListener('click', () => {
   modalOverlay.classList.remove('is-open');
@@ -13,5 +19,28 @@ modalCloseBtn.addEventListener('click', () => {
 modalOverlay.addEventListener('click', event => {
   if (event.target === modalOverlay) {
     modalOverlay.classList.remove('is-open');
+  }
+});
+
+if (mobileMenuBtn) {
+  mobileMenuBtn.addEventListener('click', () => {
+    mobileOverlay.classList.add('is-open');
+  });
+}
+
+mobileCloseBtn.addEventListener('click', () => {
+  mobileOverlay.classList.remove('is-open');
+});
+
+mobileOverlay.addEventListener('click', event => {
+  if (event.target === mobileOverlay) {
+    mobileOverlay.classList.remove('is-open');
+  }
+});
+
+window.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    modalOverlay.classList.remove('is-open');
+    mobileOverlay.classList.remove('is-open');
   }
 });
